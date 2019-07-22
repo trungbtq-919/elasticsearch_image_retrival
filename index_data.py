1cc5f903a82a356aebc58becd121708cc231f784
trungbtq-919 <tran.quang.trungb@sun-asterisk.com>
2019-07-22 11:41:26 +0700

    edit search class

--- index_data.py
+++ index_data.py
@@ -4,30 +4,31 @@ import argparse
 import json
 # import json_generator as JSON_Generator
 
+
 def gen_index_data(num_groups, num_clusters):
 
 	 # Load index json file
-	 file_path = "./encode_results/encode_" + str(num_groups) \
-	 			+ "groups_" + str(num_clusters) + "clusters/" \
-	 			+ "encode_" + str(num_groups) + "groups_" \
-	 			+ str(num_clusters) + "clusters" + ".json"
-	 
-	 # Open file
-	 with open(file_path) as f:
-	 	json_index_file = json.load(f)
+	file_path = "./encode_results/encode_" + str(num_groups) \
+				+ "groups_" + str(num_clusters) + "clusters/" \
+				+ "encode_" + str(num_groups) + "groups_" \
+				+ str(num_clusters) + "clusters" + ".json"
 
-	 for idx, document in enumerate(json_index_file):
-	 	# Convert dict to JSON-based string
-	 	document = json.dumps(document)
-	 	
-	 	# generate each document for indexing
-	 	yield {
-	 		"op_type": "index",
-	 		"_index": "face_off",
-	 		"_type": "_doc",
-	 		"_id": str(idx),
-	 		"_source": document
-	 	}
+	 # Open file
+	with open(file_path) as f:
+		json_index_file = json.load(f)
+
+	for idx, document in enumerate(json_index_file):
+		# Convert dict to JSON-based string
+		document = json.dumps(document)
+
+		# generate each document for indexing
+		yield {
+			"op_type": "index",
+			"_index": "face_off",
+			"_type": "_doc",
+			"_id": str(idx),
+			"_source": document
+		}
 
 def main():
 	"""
@@ -38,17 +39,16 @@ def main():
 		"port": port
 		})
 	"""
-	parser = argparse.ArgumentParser("Index data to Elasticsearch")
-	parser.add_argument("--num_groups", type=int, required=True)
-	parser.add_argument("--num_clusters", type=int, required=True)
-	args = parser.parse_args()
+
+	num_groups = 16
+	num_clusters = 16
 
 	host = "localhost"
 	port = "9200"
 	es = Elasticsearch(host + ":" + port)
 	
 	# Index
-	bulk(es, gen_index_data(args.num_groups, args.num_clusters)
+	bulk(es, gen_index_data(num_groups, num_clusters))
 
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -4,30 +4,90 @@ Author: Gia Huy
 """
 
 from elasticsearch import Elasticsearch
+from data_encoder import DataEncoder
 import numpy as np
 import csv
 import json
 
+
+class Searcher(object):
+
+	def __init__(self, num_returns, num_groups, num_clusters, query_vector):
+		self.num_returns = num_returns
+		self.es = Elasticsearch("localhost:9200")
+		self.num_groups = num_groups
+		self.num_clusters = num_clusters
+		self.data_encoder = DataEncoder(num_groups, num_clusters, 1000, query_vector.reshape(1, query_vector.shape[0]))
+
+	def get_string_tokens(self):
+		kmeans, query_string_tokens_list = self.data_encoder.encode_string_tokens()
+		return query_string_tokens_list[0]
+
+	def get_query_request_body(self, query_string_tokens):
+		string_tokens_body = list()
+		for i in range(self.num_groups):
+			sub_field = {
+				"filter": {
+					"term": {
+						"image_encoded_tokens": query_string_tokens[i]
+					}
+				},
+				"weight": 1
+			}
+
+			string_tokens_body.append(sub_field)
+
+
+		# RETRIEVE ONLY
+		request_body = {
+			"size": self.num_returns,
+			"query": {
+				"function_score": {
+					"functions": string_tokens_body,
+					"score_mode": "sum",
+					"boost_mode": "replace"
+				}
+			}
+		}
+
+		return request_body
+
+	def search(self, request_body):
+
+		res = es.search(index="face_off", body=request_body)
+
+		# Print Results in console.
+		for result in res['hits']['hits']:
+			print(result['_source']['id'])
+
 def main():
 	# Elasticsearch client.
-	es = Elasticsearch("192.168.19.71:9200")
+	es = Elasticsearch("localhost:9200")
 
 	# For testing
 	# Query vector
 	training_embedding_vectors = np.load("train_embs.npy")
-	query_vector = training_embedding_vectors[0] # 1-st vector
-	query_string_tokens = list()
-	
-	path = "./encode_results" + "/" \
-			+ "encode_16groups_16clusters" + "/" \
-			+ "encoded_string_16groups_16clusters" + ".csv"
-	with open(path) as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=",")
-		for idx, line in enumerate(csv_reader):
-			line_number = idx + 1
-			if line_number == 1:
-				query_string_tokens.append(line)
-				break
+	query_vector = training_embedding_vectors[6] # 1-st vector
+
+	data_encoder = DataEncoder(16, 16, 1000, query_vector.reshape(1, query_vector.shape[0]))
+	kmeans, query_string_tokens_list = data_encoder.encode_string_tokens()
+	query_string_tokens = query_string_tokens_list[0]
+	print(query_string_tokens)
+
+	# query_string_tokens = list()
+	#
+	# path = "./encode_results" + "/" \
+	# 		+ "encode_16groups_16clusters" + "/" \
+	# 		+ "encoded_string_16groups_16clusters" + ".csv"
+	#
+	# with open(path) as csv_file:
+	# 	csv_reader = csv.reader(csv_file, delimiter=",")
+	# 	for idx, line in enumerate(csv_reader):
+	# 		line_number = idx + 1
+	# 		if line_number == 7:
+	# 			print(line)
+	# 			query_string_tokens = line
+	# 			break
 
 	#print(type(query_string_tokens[0][0])) # For debug
 	string_tokens_body = list()
@@ -36,7 +96,7 @@ def main():
 		sub_field = {
 			"filter": {
 				"term": {
-					"string_token": query_string_tokens[0][i]
+					"image_encoded_tokens": query_string_tokens[i]
 				}
 			},
 			"weight": 1
@@ -45,44 +105,6 @@ def main():
 		string_tokens_body.append(sub_field)
 
 	s = 20
-	r = 20
-	request_body_1 = {
-		"size": s,
-		"query": {
-			"function_score": {
-				"functions": string_tokens_body,
-				"score_mode": "sum",
-				"boost_mode": "replace"
-			}
-		},
-		"rescore": {
-			"window_size": r,
-			"query": {
-				"rescore_query": {
-					"function_score": {
-						"script_score": {
-							"script": {
-								"lang": "painless",
-								"source": """
-									double sum = 0.0 ;
-									for (int index = 0; index < doc['embedding_vector'].length; index++) {
-										sum += Math.pow(params.query_vector[index] - doc['embedding_vector'][index], 2);
-									}
-									return(Math.sqrt(sum));
-								""",
-								"params": {
-									"query_vector": query_vector.tolist()	
-								}
-							}
-						},
-						"boost_mode": "replace"
-					}
-				},
-				"query_weight": 0,
-				"rescore_query_weight": 1
-			}
-		}
-	}
 
 	# RETRIEVE ONLY
 	request_body_2 = {
@@ -100,7 +122,8 @@ def main():
 	res = es.search(index="face_off", body=request_body_2)
 	
 	# Print Results in console.
-	print(json.dumps(res, indent=2)) # For debug
+	for result in res['hits']['hits']:
+		print(result['_source']['id'])
 
 if __name__ == "__main__":
 	main()
